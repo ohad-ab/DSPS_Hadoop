b2c3422f2a6886bac2ac50498c6efb826d3d378f
Ori Suchy <orisuchy@gmail.com>
2021-12-15 20:40:32 +0200

    init main file

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+//        Link to Google hebrew 3-Grams in S3
+//        s3://datasets.elasticmapreduce/ngrams/books/20090715/heb-all/3gram/data
+        
+        System.out.println("Hello");
+    }
+}
