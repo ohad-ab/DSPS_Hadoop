d1219a5bf9b0027facaf122185e479b9d11fa62e
Ori <suchy@post.bgu.ac.il>
2022-01-17 11:34:15 +0200

    step1 - finish?

--- src/main/java/NgramWordCount_step2.java
+++ src/main/java/NgramWordCount_step2.java
@@ -1,6 +1,5 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -10,16 +9,13 @@ import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
-import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 import static java.lang.Integer.parseInt;
 
-public class NgramWordCount {
+public class NgramWordCount_step2 {
 
 
 public static class MapperClass extends Mapper<LongWritable, Text, Text, IntWritable> {
@@ -37,7 +33,7 @@ public static class MapperClass extends Mapper<LongWritable, Text, Text, IntWrit
           Text w2 = new Text(entryWords.split(" ")[1]);
           Text w3 = new Text(entryWords.split(" ")[2]);
           Text w1w2 = new Text(w1 + "_" + w2 + "_*");
-          Text w2w3 = new Text("*_" + w2 + "_" + w3);
+//          Text w2w3 = new Text(w2 + "_" + w3);
           Text w1w2w3 = new Text(w1 + "_" + w2 + "_" + w3);
 //          context.write(w3, occ);
 //          context.write(w2w3, occ);
@@ -58,15 +54,15 @@ public static class MapperClass extends Mapper<LongWritable, Text, Text, IntWrit
     @Override
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException,  InterruptedException {
         int N3 = 0;
-        Integer c0 = 0;
-        Double k3;
+        int c0 = 0;
+        double k3;
         if(key.toString().equals("c0")) {
             for (IntWritable value : values) {
                 c0 = c0 + value.get();
             }
 //            MultipleOutputs outputs =new MultipleOutputs<Text,Text>(context);
 //            outputs.write("c0", ,key,new Text(c0.toString()));
-            context.write(key,new Text(c0.toString()));
+            context.write(key,new Text(Integer.toString(c0)));
         }
         else {
             for (IntWritable value : values) {
@@ -75,14 +71,16 @@ public static class MapperClass extends Mapper<LongWritable, Text, Text, IntWrit
 //      String star = key.toString().substring(key.getLength()-1));
 //      System.out.println("This is: " + star);
             //System.out.println(key.toString().charAt(key.toString().length() - 1));
-            if (key.toString().charAt(key.toString().length() - 1) == '*')
+            if (key.toString().charAt(key.toString().length() - 1) == '*') {
                 c2 = N3;
+                context.write(key,new Text(Integer.toString(N3)));
+            }
             else {
 //                k3 = (Math.log(N3 + 1) + 1) / (Math.log(N3 + 1) + 2);
                 k3 = (log2(N3 + 1) + 1) / (log2(N3 + 1) + 2);
-                Double val = k3 * N3 / c2;
-                c0++;
-                context.write(key, new Text(val.toString()));
+                double val = k3 * N3 / c2;
+//                c0++;
+                context.write(key, new Text(Double.toString(val)+"\t"+Double.toString(1-k3)));
             }
         }
     }
@@ -98,7 +96,7 @@ public static class MapperClass extends Mapper<LongWritable, Text, Text, IntWrit
  public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     Job job = new Job(conf, "word count");
-    job.setJarByClass(NgramWordCount.class);
+    job.setJarByClass(NgramWordCount_step2.class);
     job.setMapperClass(MapperClass.class);
     job.setPartitionerClass(PartitionerClass.class);
     //job.setCombinerClass(ReducerClass.class);
