956d68422b87b2eb503e54d46f6064185d6976bf
Ori Suchy <orisuchy@gmail.com>
2022-01-19 13:04:54 +0200

    finish main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,25 +16,32 @@ import java.io.IOException;
 
 class currUser {
     public String bucketName;
-    public String jarPath;
+    public String step1JarPath;
+    public String step2JarPath;
+    public String step3JarPath;
+    public String step4JarPath;
 
     public currUser(String name){
         if (name.equals("Ori")) {
             this.bucketName = "oo-dspsp-ass2";
-//            this.jarPath = "s3://oo-dspsp-ass2/WordCount.jar";
-            this.jarPath = "s3://oo-dspsp-ass2/NgramWordCount.jar";
         }
-        else{
+        else {
             this.bucketName = "dsps-221";
-            this.jarPath = "s3://dsps-221/WordCount.jar";
         }
+            this.step1JarPath = "s3://"+ bucketName +"/step1.jar";
+            this.step2JarPath = "s3://"+ bucketName +"/step2.jar";
+            this.step3JarPath = "s3://"+ bucketName +"/step3.jar";
+            this.step4JarPath = "s3://"+ bucketName +"/step4.jar";
     }
 }
 
 public class Main {
     static currUser user = new currUser("Ori");
     static String bucketName = user.bucketName;
-    static String jarPath = user.jarPath;
+    static String step1JarPath = user.step1JarPath;
+    static String step2JarPath = user.step2JarPath;
+    static String step3JarPath = user.step3JarPath;
+    static String step4JarPath = user.step4JarPath;
 
     public static AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
 
@@ -42,37 +49,73 @@ public class Main {
     //        Link to Google hebrew 3-Grams in S3
     static String nGramsPath = "s3://datasets.elasticmapreduce/ngrams/books/20090715/heb-all/3gram/data";
 
-    public static void main(String[] args){
-        FromTask(jarPath);
-    }
+    public static void main(String[] args) {
 
-    public static void FromTask(String jarPath){
 //        AWSCredentials credentials = new AWSCredentials;
         EmrClient mapReduce = EmrClient.builder().credentialsProvider(credentialsProvider).region(Region.US_EAST_1).build();
-        HadoopJarStepConfig hadoopJarStep = HadoopJarStepConfig.builder()
-                .jar(jarPath)
-                .mainClass("NgramWordCount")
-                .args("s3n://"+bucketName+"/input/", "s3n://"+bucketName+"/output/")
+
+//        Step1
+        HadoopJarStepConfig hadoopJarStep1 = HadoopJarStepConfig.builder()
+                .jar(step1JarPath)
+                .mainClass("Step1")
+                .args(nGramsPath, "s3n://" + bucketName + "/output1/")
                 .build();
-        StepConfig stepConfig = StepConfig.builder()
-                .name("step_wordCount")
-                .hadoopJarStep(hadoopJarStep)
+        StepConfig stepConfig1 = StepConfig.builder()
+                .name("step1")
+                .hadoopJarStep(hadoopJarStep1)
                 .actionOnFailure("TERMINATE_JOB_FLOW")
                 .build();
+
+//        Step2
+        HadoopJarStepConfig hadoopJarStep2 = HadoopJarStepConfig.builder()
+                .jar(step2JarPath)
+                .mainClass("Step2")
+                .args("s3n://" + bucketName + "/output1/", "s3n://" + bucketName + "/output2/")
+                .build();
+        StepConfig stepConfig2 = StepConfig.builder()
+                .name("step2")
+                .hadoopJarStep(hadoopJarStep2)
+                .actionOnFailure("TERMINATE_JOB_FLOW")
+                .build();
+
+//        Step3
+        HadoopJarStepConfig hadoopJarStep3 = HadoopJarStepConfig.builder()
+                .jar(step3JarPath)
+                .mainClass("Step3")
+                .args("s3n://" + bucketName + "/output2/", "s3n://" + bucketName + "/output3/")
+                .build();
+        StepConfig stepConfig3 = StepConfig.builder()
+                .name("step3")
+                .hadoopJarStep(hadoopJarStep3)
+                .actionOnFailure("TERMINATE_JOB_FLOW")
+                .build();
+
+//        Step4
+        HadoopJarStepConfig hadoopJarStep4 = HadoopJarStepConfig.builder()
+                .jar(step4JarPath)
+                .mainClass("Step4")
+                .args("s3n://" + bucketName + "/output3/", "s3n://" + bucketName + "/output4/")
+                .build();
+        StepConfig stepConfig4 = StepConfig.builder()
+                .name("step4")
+                .hadoopJarStep(hadoopJarStep4)
+                .actionOnFailure("TERMINATE_JOB_FLOW")
+                .build();
+
         JobFlowInstancesConfig instances = JobFlowInstancesConfig.builder()
                 .instanceCount(2)
-
                 .masterInstanceType("m4.large") //TODO: check what is the free one
                 .slaveInstanceType("m4.large")
                 .hadoopVersion("2.6.0")//.ec2KeyName("key1")
                 .keepJobFlowAliveWhenNoSteps(false)
                 .build();
 //                .placement(PlacementType.builder().region(Region.US_EAST_1).build()); //TODO: check if needed
+
         RunJobFlowRequest runFlowRequest = RunJobFlowRequest.builder()
                 .name("3gram statistics")
                 .releaseLabel("emr-5.34.0")
                 .instances(instances)
-                .steps(stepConfig)
+                .steps(stepConfig1, stepConfig2, stepConfig3, stepConfig4)
                 .serviceRole("EMR_DefaultRole")
                 .jobFlowRole("EMR_EC2_DefaultRole")
                 .logUri("s3n://" + bucketName + "/logs/")
@@ -80,11 +123,5 @@ public class Main {
         RunJobFlowResponse runJobFlowResult = mapReduce.runJobFlow(runFlowRequest);
         String jobFlowId = runJobFlowResult.jobFlowId();
         System.out.println("Run job flow with id: " + jobFlowId);
-    }
-    public static void ReadNgrams() throws IOException {
-        Configuration conf = new Configuration();
-        Job job = new Job(conf,"readNgrams");
-
-        job.setInputFormatClass(SequenceFileInputFormat.class);
     }
 }
